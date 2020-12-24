cfcde68890aa6b0cfe3ebf1e0072b6bdbf85f17f
Vinicius <viniciusp.olivera@gmail.com>
2020-12-24 20:05:41 -0300

    Renamed file from disassembler.c to 8080.c

--- 8080.c
+++ 8080.c
@@ -281,5 +281,26 @@ int Disassembler(unsigned char *codebuffer, int pc) {
 }
 
 int main(int argc, char *argv[]) {
-  return 0;
+   FILE *f = fopen(argv[1], "rb");
+   if (f==NULL) {
+      printf("Error: couldn't open %s\n", argv[1]);
+      exit(1);
+   }
+   
+   fseek(f, 0L, SEEK_END);
+   int fsize = ftell(f);
+   fseek(f, 0L, SEEK_SET);
+   
+   unsigned char *buffer=malloc(fsize);
+   
+   fread(buffer, fsize, 1, f);
+   fclose(f);
+   
+   int pc = 0;
+   
+   while (pc < fsize) {
+      pc += Disassembler(buffer, pc);
+   }
+
+   return 0;
 }
