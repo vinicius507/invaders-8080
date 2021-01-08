301d840f390ac96335620fa53e1b644e1dd984e4
Vinicius <viniciusp.olivera@gmail.com>
2021-01-07 21:52:27 -0300

    Gotta find out how to load video memory buffer into SDL

--- machine.c
+++ machine.c
@@ -8,37 +8,71 @@
 const int SCREEN_WIDTH = 256;
 const int SCREEN_HEIGHT = 224;
 
+typedef struct Display {
+  SDL_Window* window;
+  SDL_Surface* screen;
+} Display;
+
+typedef struct Machine {
+  State* state;
+  Display display;
+} Machine;
+
+void screenInit(Machine* machine) {
+}
+
+Machine* InitMachine() {
+  Machine* machine = calloc(1, sizeof(Machine));
+
+  machine->state = InitState();
+  machine->display.window = NULL;
+  machine->display.screen = NULL;
+
+  return machine;
+}
+
 int main (int argc, char* argv[]) {
-  State* state = InitState();
-  ReadFile(state, argv[1]);
+  Machine* machine = InitMachine();
+  ReadFile(machine->state, argv[1]); 
   
-  int done;
   int instruction_num = 0;
-  do {
-    instruction_num++;
-    printf("Instruction: %d\n", instruction_num);
-    done = Emulate(state);
-    if (DEBUG){
-      printf("\ta %02X bc %02X%02X de %02X%02X hl %02X%02X pc %04X sp %04X memory[sp] %04X\n"
-            , state->a
-            , state->b
-            , state->c
-            , state->d
-            , state->e
-            , state->h
-            , state->l
-            , state->pc
-            , state->sp
-            , ((state->memory[state->sp+1]<<8) | (state->memory[state->sp]-2)));
-      printf("\t%s %s %s %s %s %s\n"
-            , state->cc.z ? "z" : "."
-            , state->cc.s ? "s" : "."
-            , state->cc.cy ? "cy" : "."
-            , state->cc.p ? "p" : "."
-            , state->cc.ac ? "ac" : "."
-            , state->int_enable ? "i" : ".");
+  int quit = 0;
+  SDL_Event e;
+  while(!quit) {
+    int cycles = Emulate(machine->state);
+    while(SDL_PollEvent(&e) != 0) {
+      if (e.type == SDL_QUIT)
+        quit = 1;
     }
-  } while(done != 0);
+
+    if (DEBUG){
+      instruction_num++;
+      printf("Instruction: %d\n", instruction_num);
+      printf("\ta %02X bc %02X%02X de %02X%02X hl %02X%02X pc %04X sp %04X memory[sp] %04X\n"
+            , machine->state->a
+            , machine->state->b
+            , machine->state->c
+            , machine->state->d
+            , machine->state->e
+            , machine->state->h
+            , machine->state->l
+            , machine->state->pc
+            , machine->state->sp
+            , ((machine->state->memory[machine->state->sp+1]<<8) | (machine->state->memory[machine->state->sp]-2)));
+      printf("\t%s %s %s %s %s %s\n"
+            , machine->state->cc.z ? "z" : "."
+            , machine->state->cc.s ? "s" : "."
+            , machine->state->cc.cy ? "cy" : "."
+            , machine->state->cc.p ? "p" : "."
+            , machine->state->cc.ac ? "ac" : "."
+            , machine->state->int_enable ? "i" : ".");
+    }
+  }
+  
+  SDL_FreeSurface(machine->display.screen);
+  machine->display.screen = NULL;
+  SDL_DestroyWindow(machine->display.window);
+  SDL_Quit();
   
   return 0;
 }
